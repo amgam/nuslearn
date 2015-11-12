cec54ebe6ba48c9dad2f9c9e3e699739debc9e78
Tricia Tan <tricia.tzy@gmail.com>
2015-11-12 22:54:46 +0800

    uncomment

--- angular_flask/controllers.py
+++ angular_flask/controllers.py
@@ -7,7 +7,7 @@ from flask import Flask, request, Response
 from flask import render_template, url_for, redirect, send_from_directory
 from flask import send_file, make_response, abort
 
-# from angular_flask import app
+from angular_flask import app
 
 ####### CUSTOM CLASSES & DATA ######
 from learner import Learner
